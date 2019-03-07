1691afc9ce812647255387c4cf51306ecd1da920
Ignacio Gomez <ignacio.gomez@prodyna.com>
2019-03-07 09:18:00 +0100

    lowest possible value not being overriden by 0

--- hackerrank/2d-array/main.go
+++ hackerrank/2d-array/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	arr := [][]int{
-		[]int{-9, -9, -9, 1, 1, 1},
-		[]int{0, -9, 0, 4, 3, 2},
-		[]int{-9, -9, -9, 1, 2, 3},
-		[]int{0, 0, 8, 6, 6, 0},
-		[]int{0, 0, 0, -2, 0, 0},
-		[]int{0, 0, 1, 2, 4, 0},
+	arr := [][]int32{
+		[]int32{-9, -9, -9, 1, 1, 1},
+		[]int32{0, -9, 0, 4, 3, 2},
+		[]int32{-9, -9, -9, 1, 2, 3},
+		[]int32{0, 0, 8, 6, 6, 0},
+		[]int32{0, 0, 0, -2, 0, 0},
+		[]int32{0, 0, 1, 2, 4, 0},
 	}
-	maxCount := 0
+	maxCount := int32(-9 * 7)
 	for i := 0; i < len(arr)-2; i++ {
 		for j := 0; j < len(arr)-2; j++ {
-			currentSum := 0
+			currentSum := int32(0)
 			//upper row
 			currentSum += arr[i][j]
 			currentSum += arr[i][j+1]
