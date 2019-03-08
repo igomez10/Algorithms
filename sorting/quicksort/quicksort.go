ea255c54c1f312cf5426b6a40d629a38ccd650b4
Ignacio Gomez <ignacio.gomez@prodyna.com>
2019-03-08 17:40:11 +0100

    Critical bug fixes for quicksort, first element was constant

--- sorting/quicksort/quicksort.go
+++ sorting/quicksort/quicksort.go
@@ -6,40 +6,50 @@ import (
 
 func main() {
 	arr := []int{5, 8, 7, 6, 4, 3, 2, 1}
+	fmt.Printf("Initial Array: %+v\n", arr)
 
 	sort(&arr, 0, len(arr)-1)
 
-	fmt.Println(arr)
-	fmt.Println("EOF")
+	fmt.Printf("Sorted Array: %+v\n", arr)
 }
 
 func sort(arr *[]int, lo, hi int) {
 	if lo >= hi {
 		return
 	}
-	sortedIndex := partition(arr, lo, hi)
-	partition(arr, lo, sortedIndex)
-	partition(arr, sortedIndex, hi)
+
+	finalIndexForFirstE := partition(arr, lo, hi)
+	//first element of the array is already placed in the final position
+
+	sort(arr, lo, finalIndexForFirstE-1) // the first if in sort() will handle any out of bounds from the -1 +1
+	sort(arr, finalIndexForFirstE+1, hi)
 }
 
 func partition(arr *[]int, lo, hi int) int {
-	if len(*arr) <= 1 || lo >= hi {
-		return 0
+
+	//decrease hi until hits a smaller num (k < arr[0])
+	//increase lo until hits a bigger num (k > arr[0]
+	// switch arr[lo] with arr[hi]
+	// continue decreasing hi and increasing lo until lo >= hi
+	// when this happens, switch arr[hi] with arr[0], the latter will be in its final position
+
+	pivot := (*arr)[lo]
+	smallPointer := lo + 1
+	bigPointer := hi
+
+	for smallPointer < bigPointer {
+		for (*arr)[smallPointer] <= pivot && smallPointer < hi {
+			smallPointer++
+		}
+
+		for (*arr)[bigPointer] >= pivot && bigPointer > lo {
+			bigPointer--
+		}
+
+		(*arr)[bigPointer], (*arr)[smallPointer] = (*arr)[smallPointer], (*arr)[bigPointer]
 	}
-	pivotElement := (*arr)[0]
-	for {
-		for (*arr)[lo] < pivotElement && lo <= hi {
-			lo++
-		}
-		for (*arr)[hi] > pivotElement && hi >= lo {
-			hi--
-		}
-		if hi <= lo {
-			break
-		} else {
-			(*arr)[lo], (*arr)[hi] = (*arr)[hi], (*arr)[lo]
-		}
-	}
-	(*arr)[hi], (*arr)[0] = (*arr)[0], (*arr)[hi]
-	return hi
+
+	(*arr)[smallPointer], (*arr)[lo] = (*arr)[lo], (*arr)[smallPointer]
+
+	return bigPointer
 }
