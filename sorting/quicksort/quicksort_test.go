726e1e7b3a6812faa0faa9012929e451d6c60155
Ignacio Gomez <ignacio.gomez@prodyna.com>
2019-03-10 03:21:23 +0100

    more test cases

--- sorting/quicksort/quicksort_test.go
+++ sorting/quicksort/quicksort_test.go
@@ -4,12 +4,20 @@ import "testing"
 
 // TestQuickSort tests the manual implementation of quicksort
 func TestQuickSort(t *testing.T) {
-	arr := []int{5, 8, 7, 6, 4, 3, 2, 1}
-	QuickSort(&arr, 0, len(arr)-1)
+	cases := [][]int{
+		[]int{5, 8, 7, 6, 4, 3, 2, 1},
+		[]int{9, 8, 7, 6, 5, 4, 3, 2, 1},
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+		[]int{9, 1, 2, 3, 4, 5, 6, 7, 8},
+	}
+	for _, arr := range cases {
+		QuickSort(&arr, 0, len(arr)-1)
 
-	for i := 1; i < len(arr); i++ {
-		if arr[i] < arr[i-1] {
-			t.Errorf("Array is not sorted: %d is bigger than %d  ----- sortedArray: %+v", arr[i-1], arr[i], arr)
+		for i := 1; i < len(arr); i++ {
+			if arr[i] < arr[i-1] {
+				t.Errorf("Array is not sorted: %d is bigger than %d  ----- sortedArray: %+v", arr[i-1], arr[i], arr)
+			}
 		}
 	}
 }
