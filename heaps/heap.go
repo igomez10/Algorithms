8ad4bfa4fedc7d4ac33d2793d7cc90940f783136
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2019-03-21 21:24:52 -0500

    recursive sinking, fixed printing, now deleteMin

--- heaps/heap.go
+++ heaps/heap.go
@@ -4,20 +4,40 @@ import "fmt"
 import "math"
 import "strings"
 
+/*
+Build max heap from unordered array
+Find max element a[i]
+swap elements a[n] a[i] now max element is at the end of array
+discard node n from heap, decrementing heap size
+new root after the swap may violate max heap property but its children are max heaps, we can run max heapify to fix this
+*/
+
 func main() {
-	values := []int{1, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println("Initial Array:")
+	values := []int{-1, 1, 3, 4, 5, 6, 7, 8, 9}
+	deleteMin(&values)
 	printHeap(values)
-	inserted := 2
-	insert(&values, inserted)
-	fmt.Printf("After inserting %d:\n", inserted)
-	printHeap(values)
-	fmt.Printf("Heap is sorted: %t", isSorted(values))
+	//	fmt.Println("Initial Array:")
+	//	printHeap(values)
+	//	inserted := 2
+	//	insert(&values, inserted)
+	//	fmt.Printf("After inserting %d:\n", inserted)
+	//	printHeap(values)
+	//	fmt.Printf("Heap is sorted: %t\n", isSorted(values))
 }
 
-func printHeap(arr []int) {
+func heapSort(arr *[]int) {
 
-	levelsInArr := int(math.Log2(float64(len(arr))))
+}
+
+func deleteMin(arr *[]int) {
+	(*arr)[1] = (*arr)[len(*arr)-1]
+	*arr = (*arr)[:len(*arr)-1]
+	sink(arr, 1)
+}
+
+func printHeap(heap []int) {
+	arr := heap[1:]
+	levelsInArr := int(math.Ceil(math.Log2(float64(len(arr)))))
 	for i := 0; i <= levelsInArr; i++ {
 		var row []int
 		index := float64(i)
@@ -30,7 +50,9 @@ func printHeap(arr []int) {
 		}
 
 		initalSpace := strings.Repeat(" ", (levelsInArr*levelsInArr)/2-i)
-		fmt.Println(initalSpace, row)
+		if len(row) > 0 {
+			fmt.Println(initalSpace, row)
+		}
 	}
 }
 
@@ -46,20 +68,62 @@ func isSorted(arr []int) bool {
 	return isSorted
 }
 
+// sink takes the given index and moves
+func sink(arr *[]int, indexOfValue int) int {
+	// if a value is in last row do nothing
+	i := indexOfValue
+	isALeaf := i >= len(*arr)/2
+	isInFinalPosition := isALeaf || (((*arr)[i] <= (*arr)[i*2]) && (*arr)[i] <= (*arr)[i*2+1])
+	// the element is in its final position if its a leaf or if its smaller than both childs
+	//isInFinalPosition := isALeaf || (((*arr)[i] <= (*arr)[i*2]) && (*arr)[i] <= (*arr)[i*2+1])
+	for !isInFinalPosition {
+		left := (*arr)[i*2]
+		right := (*arr)[i*2+1]
+		var smallest int
+		if right > left {
+			smallest = i * 2
+		} else {
+			smallest = i*2 + 1
+		}
+		(*arr)[i], (*arr)[smallest] = (*arr)[smallest], (*arr)[i]
+		i = smallest
+		isALeaf = i >= len(*arr)/2
+		isInFinalPosition = isALeaf || (((*arr)[i] <= (*arr)[i*2]) && (*arr)[i] <= (*arr)[i*2+1])
+	}
+
+	return i
+}
+
+func isInHeap(arr []int, elementToFind int) bool {
+	i := 0
+	for i < len(arr) {
+		if arr[i] == elementToFind {
+			return true
+		} else if arr[i*2] < elementToFind {
+			i = i*2 + 1
+		} else {
+			i = i * 2
+		}
+	}
+	return false
+}
+
 func swim(arr *[]int, indexOfValue int) {
 	i := indexOfValue
-
 	//if index is root or if parent is smaller than Value-> do nothing
-	valueIsInFinalPosition := i == 0 || (*arr)[i/2] < (*arr)[i]
-	if !valueIsInFinalPosition {
+
+	isRoot := i == 0
+	valueIsInFinalPosition := isRoot || (*arr)[i/2] <= (*arr)[i]
+	for !valueIsInFinalPosition {
 		(*arr)[i], (*arr)[i/2] = (*arr)[i/2], (*arr)[i]
-		swim(arr, i/2)
+		i = i / 2
+		isRoot = i == 0
+		valueIsInFinalPosition = isRoot || (*arr)[i/2] < (*arr)[i]
 	}
 }
 
 func insert(arr *[]int, newValue int) {
 	*arr = append(*arr, newValue)
-	//parent := (*arr)[len(*arr)/2]
 	if ok := isSorted(*arr); !ok {
 		swim(arr, len(*arr)-1)
 	}
--- heaps/heap_test.go
+++ heaps/heap_test.go
@@ -2,6 +2,19 @@ package main
 
 import "testing"
 
+func TestDeleteMin(t *testing.T) {
+
+	values := []int{-1, 1, 2, 3, 4, 5, 6, 7}
+	deleteMin(&values)
+	expectedArray := []int{-1, 2, 4, 3, 7, 5, 6}
+	for i := 0; i < len(values); i++ {
+		if values[i] != expectedArray[i] {
+			t.Errorf("Expected %d got %d\n", expectedArray[i], values[i])
+		}
+	}
+
+}
+
 func TestIsSorted(t *testing.T) {
 	goodValues := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	if ok := isSorted(goodValues); !ok {
@@ -24,5 +37,63 @@ func TestInsert(t *testing.T) {
 	if len(aHeap) < initialLength {
 		m := "New value %d should be bigger than old value %d"
 		t.Error(m, len(aHeap), initialLength)
+	}
+}
+
+func sameSizeArray(t *testing.T, arrA, arrB []int) bool {
+	sameSize := true
+	if len(arrA) != len(arrB) {
+		sameSize = false
+		m := `Length of array is not the same: %d was %d`
+		t.Errorf(m, len(arrA), len(arrB))
+	}
+	return sameSize
+}
+
+func sameElementsInArray(t *testing.T, arrA, arrB []int) bool {
+	hasSameElements := true
+	if sameSizeArray(t, arrA, arrB) {
+		for i := 0; i < len(arrA); i++ {
+			if arrA[i] != arrB[i] {
+				hasSameElements = false
+				t.Errorf("Values changed in balanced heap: %d, %d", arrA[i], arrB[i])
+				break
+			}
+		}
+	}
+	return hasSameElements
+}
+
+func TestSink(t *testing.T) {
+	goodHeap := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	copyOfHeap := goodHeap
+	//try swim with each possible value in heap
+	for i := 0; i < len(goodHeap); i++ {
+		sink(&goodHeap, i)
+	}
+
+	sameElementsInArray(t, goodHeap, copyOfHeap)
+
+	badValues := []int{1, 3, 4, 5, 6, 7, 8, 3}
+	swim(&badValues, len(badValues)-1)
+	if !isSorted(badValues) {
+		t.Errorf("Heap %+v should be sorted after swim operation", badValues)
+	}
+}
+
+func TestSwim(t *testing.T) {
+	goodHeap := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	copyOfHeap := goodHeap
+	//try swim with each possible value in heap
+	for i := 0; i < len(goodHeap); i++ {
+		swim(&goodHeap, i)
+	}
+
+	sameElementsInArray(t, goodHeap, copyOfHeap)
+
+	badValues := []int{1, 3, 4, 5, 6, 7, 8, 3}
+	swim(&badValues, len(badValues)-1)
+	if !isSorted(badValues) {
+		t.Errorf("Heap %+v should be sorted after swim operation", badValues)
 	}
 }
