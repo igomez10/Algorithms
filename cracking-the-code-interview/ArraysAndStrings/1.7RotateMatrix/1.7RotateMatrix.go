670cb28a247b8bdcaa9cb59750220ce38ee0443c
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2019-03-27 19:02:06 -0500

    Rotate matrix in place

--- cracking-the-code-interview/ArraysAndStrings/1.7RotateMatrix/1.7RotateMatrix.go
+++ cracking-the-code-interview/ArraysAndStrings/1.7RotateMatrix/1.7RotateMatrix.go
@@ -9,19 +9,25 @@ func main() {
 }
 
 func RotateMatrix(arr *[][]int) {
+	for initial := 0; initial < len(*arr)/2; initial++ {
 
-	for i := 0; i < len(*arr)-1; i++ {
-		tmp := (*arr)[0][i]
+		for i := initial; i < len(*arr)-initial-1; i++ {
 
-		(*arr)[i][len(*arr)-1], tmp = tmp, (*arr)[i][len(*arr)-1]
+			tmp := (*arr)[initial][i]
 
-		(*arr)[len(*arr)-1][len(*arr)-1-i], tmp = tmp, (*arr)[len(*arr)-1][len(*arr)-1-i]
+			(*arr)[i][len(*arr)-1-initial], tmp = tmp, (*arr)[i][len(*arr)-1-initial]
 
-		(*arr)[len(*arr)-1-i][0], tmp = tmp, (*arr)[len(*arr)-1-i][0]
+			(*arr)[len(*arr)-1-initial][len(*arr)-1-i], tmp = tmp, (*arr)[len(*arr)-1-initial][len(*arr)-1-i]
 
-		(*arr)[0][i], tmp = tmp, (*arr)[0][i]
+			(*arr)[len(*arr)-1-i][initial], tmp = tmp, (*arr)[len(*arr)-1-i][initial]
+
+			(*arr)[initial][i], tmp = tmp, (*arr)[initial][i]
+		}
+
 	}
+
 }
+
 func PrintMatrix(arr [][]int) {
 	for i := range arr {
 		for j := range arr {
