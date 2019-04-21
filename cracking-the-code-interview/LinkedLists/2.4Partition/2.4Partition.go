50268c06e9180ef354e79087b8b62294e828de35
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2019-04-21 16:49:58 +0200

    2.4Patition

--- cracking-the-code-interview/LinkedLists/2.4Partition/2.4Partition.go
+++ cracking-the-code-interview/LinkedLists/2.4Partition/2.4Partition.go
@@ -1,1 +1,102 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// LinkedListNode is a node in a singly linked list, only stores reference to next value
+type LinkedListNode struct {
+	value int
+	next  *LinkedListNode
+}
+
+func main() {
+	values := []int{1, 9, 2, 8, 3, 7, 4, 6, 5}
+	head := LLFromArray(values)
+
+	partition(&head, 5)
+
+	fmt.Println(head.ToString())
+}
+
+// LLFromArray receives an array of values and returns a reference to a linked list node
+// as the first element from the array
+func LLFromArray(values []int) *LinkedListNode {
+	firstElement := LinkedListNode{value: values[0]}
+	head := &firstElement
+	for i := 1; i < len(values); i++ {
+		head.next = &LinkedListNode{value: values[i]}
+		head = head.next
+	}
+	return &firstElement
+}
+
+// ToString returns a string formatted version of the linked list
+func (l *LinkedListNode) ToString() string {
+	builder := strings.Builder{}
+
+	for l != nil {
+		builder.WriteString(strconv.Itoa(l.value))
+		l = l.next
+	}
+	return builder.String()
+}
+
+func partition(head **LinkedListNode, value int) {
+	var smallerHead *LinkedListNode
+	var smallerIterative *LinkedListNode
+
+	var biggerHead *LinkedListNode
+	var biggerIterative *LinkedListNode
+
+	var equalsHead *LinkedListNode
+	var equalsIterative *LinkedListNode
+
+	iterativeHead := *head
+
+	for iterativeHead != nil {
+
+		if (*iterativeHead).value < value {
+
+			if smallerHead == nil {
+				smallerHead = iterativeHead
+				smallerIterative = iterativeHead
+
+			} else {
+				smallerIterative.next = iterativeHead
+				smallerIterative = smallerIterative.next
+			}
+
+		} else if (*iterativeHead).value > value {
+
+			if biggerHead == nil {
+				biggerHead = iterativeHead
+				biggerIterative = iterativeHead
+
+			} else {
+				biggerIterative.next = iterativeHead
+				biggerIterative = biggerIterative.next
+			}
+
+		} else {
+
+			if equalsHead == nil {
+				equalsHead = iterativeHead
+				equalsIterative = iterativeHead
+
+			} else {
+				equalsIterative.next = iterativeHead
+				equalsIterative = equalsIterative.next
+			}
+		}
+		iterativeHead = iterativeHead.next
+	}
+
+	smallerIterative.next = equalsHead
+	equalsIterative.next = biggerHead
+	biggerIterative.next = nil
+
+	head = &smallerHead
+}
--- cracking-the-code-interview/LinkedLists/2.4Partition/2.4Partition_test.go
+++ cracking-the-code-interview/LinkedLists/2.4Partition/2.4Partition_test.go
@@ -1,1 +1,48 @@
 package main
+
+import "strings"
+import "testing"
+
+func TestLLFromArray(t *testing.T) {
+	values1 := []int{1, 1, 2, 2, 3, 4, 5, 6, 6, 7, 7, 2, 1, 2, 3, 4, 5, 6, 7}
+
+	head := LLFromArray(values1)
+
+	for i := 0; i < len(values1); i++ {
+		if values1[i] != head.value {
+			t.Errorf("Expected %d got %d", values1[i], head.value)
+		}
+		head = head.next
+	}
+}
+
+func TestLinkedListNode_ToString(t *testing.T) {
+	values := []int{1, 1, 2, 2, 3, 3, 4, 6, 5, 4}
+	head := LLFromArray(values)
+	stringValue := head.ToString()
+	expectedValue := "1122334654"
+	if strings.Compare(stringValue, expectedValue) != 0 {
+		t.Errorf("expected %s got %s", stringValue, expectedValue)
+	}
+}
+
+func TestPartition(t *testing.T) {
+
+	values := []int{1, 9, 2, 8, 3, 7, 4, 6, 5}
+	partitionValue := 5
+
+	expectedValues := []int{1, 2, 3, 4, 5, 9, 8, 7, 6}
+
+	head := LLFromArray(values)
+
+	partition(&head, partitionValue)
+
+	for i := 0; i < len(expectedValues); i++ {
+		if expectedValues[i] != head.value {
+			t.Errorf("Expected %d, got %d", expectedValues[i], head.value)
+			t.Error(head.ToString())
+		}
+		head = head.next
+	}
+
+}
