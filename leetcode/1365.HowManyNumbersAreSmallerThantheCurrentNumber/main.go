ff4b6cac6902aadff207c8ed8436371aeb16f2f9
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2020-04-28 23:17:19 +0200

    nums smaller than number

---
+++ leetcode/1365.HowManyNumbersAreSmallerThantheCurrentNumber/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	nums := []int{6, 5, 4, 8}
+	smallerNumbersThanCurrent(nums)
+}
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	occurrences := [101]int{}
+	for _, num := range nums {
+		occurrences[num]++
+	}
+	prevOccurrences := occurrences[0]
+	occurrences[0] = 0
+	for i := range occurrences[1:] {
+		occurrences[i+1], prevOccurrences = prevOccurrences, prevOccurrences+occurrences[i+1]
+	}
+	counts := make([]int, len(nums))
+	for i, num := range nums {
+		counts[i] = occurrences[num]
+	}
+	return counts
+}
