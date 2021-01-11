fe49ec0b299322f673122ce77a11cf87715c673b
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2021-01-11 18:47:05 -0500

    remove comments

--- leetcode/91.DecodeWays/decodeways.go
+++ leetcode/91.DecodeWays/decodeways.go
@@ -1,16 +1,11 @@
 package main
 
 func numDecodings(s string) int {
-	//     if len(s) == 0 || s[0] == '0' {
-	//         return 0
-	//     }
-
 	dp := make([]int, len(s)+1)
 	dp[0] = 1
 	dp[1] = 1
 	if s[0] == '0' {
-		dp[1] = 0 /// ??? if s[0] == '0' we should return 0
-		// inmediatly
+		dp[1] = 0
 	}
 
 	for i := 2; i < len(dp); i++ {
