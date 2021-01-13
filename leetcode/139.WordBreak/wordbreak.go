ee19786a3e9ff5c6081c8047687c98423c52b172
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2021-01-13 14:03:54 -0500

    word break with bfs and memoization

--- leetcode/139.WordBreak/wordbreak.go
+++ leetcode/139.WordBreak/wordbreak.go
@@ -1,27 +1,32 @@
 package main
 
+import (
+	"container/list"
+)
+
 func wordBreak(s string, dict []string) bool {
-	memo := &map[int]bool{}
-	return wordBreakMemo(s, dict, 0, memo)
-}
-
-func wordBreakMemo(s string, dict []string, index int, memo *map[int]bool) bool {
-	if index == len(s) {
-		return true
-	}
-	if val, exist := (*memo)[index]; exist {
-		return val
-	}
-
-	for i := range dict {
-		if len(dict[i]) <= len(s)-index &&
-			s[index:index+len(dict[i])] == dict[i] &&
-			wordBreakMemo(s, dict, index+len(dict[i]), memo) {
-			(*memo)[index] = true
+	l := list.New()
+	l.PushBack(0)
+	memo := map[int]bool{}
+	for l.Len() > 0 {
+		index := l.Remove(l.Front()).(int)
+		if index == len(s) {
 			return true
+		}
+
+		if _, visited := memo[index]; visited {
+			continue
+		} else {
+			memo[index] = visited
+		}
+
+		for i := range dict {
+			if len(dict[i]) <= len(s)-index &&
+				s[index:index+len(dict[i])] == dict[i] {
+				l.PushBack(index + len(dict[i]))
+			}
 		}
 	}
 
-	(*memo)[index] = false
 	return false
 }
