512953191d2c5adb73b5f915fd3c314f81f773ec
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2021-01-13 15:18:46 -0500

    wordbreak using dynamic programming

--- leetcode/139.WordBreak/wordbreak.go
+++ leetcode/139.WordBreak/wordbreak.go
@@ -1,32 +1,20 @@
 package main
 
-import (
-	"container/list"
-)
-
 func wordBreak(s string, dict []string) bool {
-	l := list.New()
-	l.PushBack(0)
-	memo := map[int]bool{}
-	for l.Len() > 0 {
-		index := l.Remove(l.Front()).(int)
-		if index == len(s) {
-			return true
-		}
+	dp := make([]bool, len(s)+1)
+	dictmap := map[string]bool{}
+	for i := range dict {
+		dictmap[dict[i]] = true
+	}
 
-		if _, visited := memo[index]; visited {
-			continue
-		} else {
-			memo[index] = visited
-		}
-
-		for i := range dict {
-			if len(dict[i]) <= len(s)-index &&
-				s[index:index+len(dict[i])] == dict[i] {
-				l.PushBack(index + len(dict[i]))
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && dictmap[s[j:i]] {
+				dp[i] = true
+				break
 			}
 		}
 	}
-
-	return false
+	return dp[len(dp)-1]
 }
