8ea09ba5b6d088f22e94ec2051f5802ee40d8152
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2021-01-28 15:19:46 -0500

    refactor minjob

--- leetcode/1335.MinimumDifficultyofaJobSchedule/minJob.go
+++ leetcode/1335.MinimumDifficultyofaJobSchedule/minJob.go
@@ -4,33 +4,33 @@ func minDifficulty(jobs []int, d int) int {
 	if len(jobs) < d {
 		return -1
 	}
-	memo := make([][]int, len(jobs))
+	memo := make([][]int, d)
 	for i := range memo {
-		memo[i] = make([]int, d)
+		memo[i] = make([]int, len(jobs))
 	}
-	val := numPartitions(jobs, d-1, 0, &memo)
+	val := numPartitions(jobs, d-1, 0, memo)
 
 	return val
 }
 
-func numPartitions(jobs []int, partitionsLeft, start int, memo *[][]int) int {
+func numPartitions(jobs []int, daysLeft, start int, memo [][]int) int {
 	if start >= len(jobs) {
 		return -1
 	}
 
-	if val := (*memo)[start][partitionsLeft]; val > 0 {
+	if val := memo[daysLeft][start]; val > 0 {
 		return val
 	}
 
-	if partitionsLeft == 0 {
+	if daysLeft == 0 {
 		mostDifficult := max(jobs[start:]...)
-		(*memo)[start][partitionsLeft] = mostDifficult
+		memo[daysLeft][start] = mostDifficult
 		return mostDifficult
 	}
 
 	minimum := 0
-	for i := start + 1; i < len(jobs)-partitionsLeft+1; i++ {
-		current := max(jobs[start:i]...) + numPartitions(jobs, partitionsLeft-1, i, memo)
+	for i := start + 1; i < len(jobs)-daysLeft+1; i++ {
+		current := max(jobs[start:i]...) + numPartitions(jobs, daysLeft-1, i, memo)
 		if minimum == 0 {
 			minimum = current
 		} else {
@@ -38,7 +38,7 @@ func numPartitions(jobs []int, partitionsLeft, start int, memo *[][]int) int {
 		}
 	}
 
-	(*memo)[start][partitionsLeft] = minimum
+	memo[daysLeft][start] = minimum
 
 	return minimum
 }
