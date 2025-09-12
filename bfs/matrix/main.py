815f3f320c3664b04578bd93c75ba7f2ebc93483
Ignacio Gomez <igomez9@dons.usfca.edu>
2025-09-11 22:34:44 -0700

    refactor: clean up whitespace and formatting in Solution class methods

--- bfs/matrix/main.py
+++ bfs/matrix/main.py
@@ -1,9 +1,9 @@
 import collections
+
 from matrix import printMatrix
 
 
-class Solution():
-
+class Solution:
     def countIslands(self, arr):
         for i, row in enumerate(arr):
             for j, item in enumerate(row):
@@ -31,25 +31,29 @@ class Solution():
             current = queue.pop()
             ci, cj = current[0], current[1]
 
-            if ci > 0 and (ci-1, cj) not in seen and arr[ci-1][cj] == 1:
-                queue.append((ci-1, cj))
-                arr[ci-1][cj] = 0
-                seen[(ci-1, cj)] = True
+            if ci > 0 and (ci - 1, cj) not in seen and arr[ci - 1][cj] == 1:
+                queue.append((ci - 1, cj))
+                arr[ci - 1][cj] = 0
+                seen[(ci - 1, cj)] = True
 
-            if cj > 0 and (ci, cj-1) not in seen and arr[ci][cj-1] == 1:
-                queue.append((ci, cj-1))
-                arr[ci][cj-1] = 0
-                seen[(ci, cj-1)] = True
+            if cj > 0 and (ci, cj - 1) not in seen and arr[ci][cj - 1] == 1:
+                queue.append((ci, cj - 1))
+                arr[ci][cj - 1] = 0
+                seen[(ci, cj - 1)] = True
 
-            if ci < len(arr)-1 and (ci+1, cj) not in seen and arr[ci+1][cj] == 1:
-                queue.append((ci+1, cj))
-                arr[ci+1][cj] = 0
-                seen[(ci+1, cj)] = True
+            if ci < len(arr) - 1 and (ci + 1, cj) not in seen and arr[ci + 1][cj] == 1:
+                queue.append((ci + 1, cj))
+                arr[ci + 1][cj] = 0
+                seen[(ci + 1, cj)] = True
 
-            if cj < len(arr[ci])-1 and (ci, cj+1) not in seen and arr[ci][cj+1] == 1:
+            if (
+                cj < len(arr[ci]) - 1
+                and (ci, cj + 1) not in seen
+                and arr[ci][cj + 1] == 1
+            ):
                 queue.append((ci, cj + 1))
-                arr[ci][cj+1] = 0
-                seen[(ci, cj+1)] = True
+                arr[ci][cj + 1] = 0
+                seen[(ci, cj + 1)] = True
 
 
 arr = [
